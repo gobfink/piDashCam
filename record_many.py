6f403c0a05fd36283acbfcb36d9e0e36c6e6088b
fedora <fedora@thinkpad.fort.net>
2018-03-03 23:47:23 -0500

    Now added rotation capability, where it deletes the oldest file if over the 10G threshold

--- record_many.py
+++ record_many.py
@@ -3,16 +3,28 @@ import picamera
 import datetime as dt
 from numpy import inf
 
+def get_fs_freespace(pathname):
+    "Gets the free space of the filesystem containing pathname"
+    stat = os.statvfs(pathname)
+    return stat.f_bfree*stat.f_bsize
+
+def get_oldest_file(pathname):
+    "Gets the oldest file in the path"
+    filename = min(os.listdir(pathname), key=lambda p: os.path.getctime(os.path.join(pathname,p)))
+    filename = os.path.join(pathname, filename)
+    return filename
+
 width = 1600
 height = 900
 frameRate = 24
-filePath = '/media/pi/PNY-128G/'
+filePath = '/media/pi/PNY-128G/data/'
 fileNameFormat = '%Y-%m-%d--%H.h264'
 timeFormat = '%Y-%m-%d %H:%M:%S'
 duration = inf
 delta_record = .5
 previewVideo = False
-
+# Threshold before deleting the oldest file
+rotateThreshold = 10000000000
 
 with picamera.PiCamera() as camera:
     camera.resolution = (width,height)
@@ -28,6 +40,8 @@ with picamera.PiCamera() as camera:
     start = dt.datetime.now()
     past_hour=dt.datetime.now().hour
     while (dt.datetime.now() - start).seconds < duration:
+        if get_fs_freespace(filePath) < rotateThreshold:
+            os.remove(get_oldest_file(filePath))
         if past_hour != dt.datetime.now().hour:
             past_hour = dt.datetime.now().hour
             current_time = dt.datetime.now()
@@ -38,7 +52,3 @@ with picamera.PiCamera() as camera:
         camera.wait_recording(delta_record)
     camera.stop_recording()
 
-def get_fs_freespace(pathname):
-    "Gets the free space of the filesystem containing pathname"
-    stat = os.statvfs(pathname)
-    return stat.f_bfree*stat.f_bsize
